6bfd8a7865226dc9b11d56921cf65fe2b27b38a7
Eu <karllhenning64@gmail.com>
2017-06-24 23:25:24 -0300

    Initial commit

---
+++ Assets/Libs/UnityNEAT/Assets/SharpNEAT/Optimizer.cs
@@ -0,0 +1,223 @@
+﻿using UnityEngine;
+using System.Collections;
+using SharpNeat.Phenomes;
+using System.Collections.Generic;
+using SharpNeat.EvolutionAlgorithms;
+using SharpNeat.Genomes.Neat;
+using System;
+using System.Xml;
+using System.IO;
+
+public class Optimizer : MonoBehaviour
+{
+
+	const int NUM_INPUTS = 3;
+	const int NUM_OUTPUTS = 2;
+
+	public int Trials;
+	public float TrialDuration;
+	public float StoppingFitness;
+	bool EARunning;
+	string popFileSavePath, champFileSavePath;
+
+	SimpleExperiment experiment;
+	static NeatEvolutionAlgorithm<NeatGenome> _ea;
+
+	public GameObject Unit;
+
+	Dictionary<IBlackBox, UnitController> ControllerMap = new Dictionary<IBlackBox, UnitController> ();
+	private DateTime startTime;
+	private float timeLeft;
+	private float accum;
+	private int frames;
+	private float updateInterval = 12;
+
+	private uint Generation;
+	private double Fitness;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Utility.DebugLog = true;
+		experiment = new SimpleExperiment ();
+		XmlDocument xmlConfig = new XmlDocument ();
+		TextAsset textAsset = (TextAsset)Resources.Load ("experiment.config");
+		xmlConfig.LoadXml (textAsset.text);
+		experiment.SetOptimizer (this);
+
+		experiment.Initialize ("My experiment", xmlConfig.DocumentElement, NUM_INPUTS, NUM_OUTPUTS);
+
+		champFileSavePath = Application.persistentDataPath + string.Format ("/{0}.champ.xml", "car");
+		popFileSavePath = Application.persistentDataPath + string.Format ("/{0}.pop.xml", "car");       
+
+		print (champFileSavePath);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		//  evaluationStartTime += Time.deltaTime;
+
+		timeLeft -= Time.deltaTime;
+		accum += Time.timeScale / Time.deltaTime;
+		++frames;
+
+		if (timeLeft <= 0.0) {
+			var fps = accum / frames;
+			timeLeft = updateInterval;
+			accum = 0.0f;
+			frames = 0;
+			//   print("FPS: " + fps);
+			if (fps < 10) {
+				Time.timeScale = Time.timeScale - 1;
+				print ("Lowering time scale to " + Time.timeScale);
+			}
+		}
+	}
+
+	public void StartEA ()
+	{        
+		Utility.DebugLog = true;
+		Utility.Log ("Starting PhotoTaxis experiment");
+		// print("Loading: " + popFileLoadPath);
+		_ea = experiment.CreateEvolutionAlgorithm (popFileSavePath);
+		startTime = DateTime.Now;
+
+		_ea.UpdateEvent += new EventHandler (ea_UpdateEvent);
+		_ea.PausedEvent += new EventHandler (ea_PauseEvent);
+
+		var evoSpeed = 25;
+
+		//   Time.fixedDeltaTime = 0.045f;
+		Time.timeScale = evoSpeed;       
+		_ea.StartContinue ();
+		EARunning = true;
+	}
+
+	void ea_UpdateEvent (object sender, EventArgs e)
+	{
+		Utility.Log (string.Format ("gen={0:N0} bestFitness={1:N6}",
+			_ea.CurrentGeneration, _ea.Statistics._maxFitness));
+
+		Fitness = _ea.Statistics._maxFitness;
+		Generation = _ea.CurrentGeneration;
+      
+
+		//    Utility.Log(string.Format("Moving average: {0}, N: {1}", _ea.Statistics._bestFitnessMA.Mean, _ea.Statistics._bestFitnessMA.Length));
+
+    
+	}
+
+	void ea_PauseEvent (object sender, EventArgs e)
+	{
+		Time.timeScale = 1;
+		Utility.Log ("Done ea'ing (and neat'ing)");
+
+		XmlWriterSettings _xwSettings = new XmlWriterSettings ();
+		_xwSettings.Indent = true;
+		// Save genomes to xml file.        
+		DirectoryInfo dirInf = new DirectoryInfo (Application.persistentDataPath);
+		if (!dirInf.Exists) {
+			Debug.Log ("Creating subdirectory");
+			dirInf.Create ();
+		}
+		using (XmlWriter xw = XmlWriter.Create (popFileSavePath, _xwSettings)) {
+			experiment.SavePopulation (xw, _ea.GenomeList);
+		}
+		// Also save the best genome
+
+		using (XmlWriter xw = XmlWriter.Create (champFileSavePath, _xwSettings)) {
+			experiment.SavePopulation (xw, new NeatGenome[] { _ea.CurrentChampGenome });
+		}
+		DateTime endTime = DateTime.Now;
+		Utility.Log ("Total time elapsed: " + (endTime - startTime));
+
+		System.IO.StreamReader stream = new System.IO.StreamReader (popFileSavePath);
+       
+
+      
+		EARunning = false;        
+        
+	}
+
+	public void StopEA ()
+	{
+
+		if (_ea != null && _ea.RunState == SharpNeat.Core.RunState.Running) {
+			_ea.Stop ();
+		} 
+	}
+
+	public void Evaluate (IBlackBox box)
+	{
+		GameObject obj = Instantiate (Unit, Unit.transform.position, Unit.transform.rotation) as GameObject;
+		UnitController controller = obj.GetComponent<UnitController> ();
+
+		ControllerMap.Add (box, controller);
+
+		controller.Activate (box);
+	}
+
+	public void StopEvaluation (IBlackBox box)
+	{
+		UnitController ct = ControllerMap [box];
+
+		Destroy (ct.gameObject);
+	}
+
+	public void RunBest ()
+	{
+		Time.timeScale = 1;
+
+		NeatGenome genome = null;
+
+
+		// Try to load the genome from the XML document.
+		try {
+			using (XmlReader xr = XmlReader.Create (champFileSavePath))
+				genome = NeatGenomeXmlIO.ReadCompleteGenomeList (xr, false, (NeatGenomeFactory)experiment.CreateGenomeFactory ()) [0];
+
+
+		} catch (Exception e1) {
+			// print(champFileLoadPath + " Error loading genome from file!\nLoading aborted.\n"
+			//						  + e1.Message + "\nJoe: " + champFileLoadPath);
+			return;
+		}
+
+		// Get a genome decoder that can convert genomes to phenomes.
+		var genomeDecoder = experiment.CreateGenomeDecoder ();
+
+		// Decode the genome into a phenome (neural network).
+		var phenome = genomeDecoder.Decode (genome);
+
+		GameObject obj = Instantiate (Unit, Unit.transform.position, Unit.transform.rotation) as GameObject;
+		UnitController controller = obj.GetComponent<UnitController> ();
+
+		ControllerMap.Add (phenome, controller);
+
+		controller.Activate (phenome);
+	}
+
+	public float GetFitness (IBlackBox box)
+	{
+		if (ControllerMap.ContainsKey (box)) {
+			return ControllerMap [box].GetFitness ();
+		}
+		return 0;
+	}
+
+	void OnGUI ()
+	{
+		if (GUI.Button (new Rect (10, 10, 100, 40), "Start Evolution")) {
+			StartEA ();
+		}
+		if (GUI.Button (new Rect (10, 60, 100, 40), "Stop Evolution")) {
+			StopEA ();
+		}
+		if (GUI.Button (new Rect (10, 110, 100, 40), "Run best")) {
+			RunBest ();
+		}
+
+		GUI.Button (new Rect (10, Screen.height - 70, 100, 60), string.Format ("Generation: {0}\nFitness: {1:0.00}", Generation, Fitness));
+	}
+}
---
+++ Assets/Scripts/CarAI.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using SharpNeat.Phenomes;
+
+enum SensorType
+{
+	Left,
+	LeftFront,
+	Front,
+	RightFront,
+	Right
+}
+
+public class CarAI : UnitController
+{
+	public bool isHumanControlled = false;
+
+	public float speed = 100f;
+	public float maxSpeed = 10f;
+	public float _currSpeed = 0f;
+
+	// Sensor parameters
+	float sensorRange = 3.0f;
+
+	// Neat
+	bool _isRunning;
+	IBlackBox _box;
+
+	// Inputs
+	public float forwardInput = 0.0f;
+	public float turnInput = 0.0f;
+
+	// Components
+	private Rigidbody _rigidbody = null;
+
+	void Start ()
+	{
+		_rigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void Update ()
+	{
+		if (isHumanControlled) {
+			forwardInput = Input.GetAxisRaw ("Vertical");
+			turnInput = Input.GetAxisRaw ("Horizontal");
+		}
+
+		//Debug.DrawRay (transform.position, transform.up * 0.3f);
+		Debug.DrawRay (transform.position, transform.up * sensorRange);
+		Debug.DrawRay (transform.position, (transform.up - transform.right).normalized * sensorRange);
+		Debug.DrawRay (transform.position, (transform.up + transform.right).normalized * sensorRange);
+	}
+
+	void FixedUpdate ()
+	{
+		if (isHumanControlled == false && _isRunning) {
+			NeatInputUpdate ();
+		}
+
+		_rigidbody.AddForce (transform.up * speed * forwardInput);
+		_rigidbody.AddTorque (-transform.forward * turnInput * 0.4f);
+		_currSpeed = _rigidbody.velocity.magnitude;
+
+		if (_rigidbody.velocity.magnitude > maxSpeed) {
+			_rigidbody.velocity = Vector3.ClampMagnitude (_rigidbody.velocity, maxSpeed);
+		}		
+	}
+
+	void NeatInputUpdate ()
+	{
+		ISignalArray neatInputs = _box.InputSignalArray;
+		neatInputs [0] = 0.0;
+		RaycastHit hit;
+
+		if (Physics.Raycast (transform.position, transform.up, out hit, sensorRange)) {
+			neatInputs [0] = 1 - hit.distance / sensorRange;
+		}
+
+		if (Physics.Raycast (transform.position, transform.up - transform.right, out hit, sensorRange)) {
+			neatInputs [1] = 1 - hit.distance / sensorRange;
+		}
+
+		if (Physics.Raycast (transform.position, transform.up + transform.right, out hit, sensorRange)) {
+			neatInputs [2] = 1 - hit.distance / sensorRange;
+		}
+
+		_box.Activate ();
+
+		ISignalArray neatOutputs = _box.OutputSignalArray;
+
+		forwardInput = (float)neatOutputs [0] * 2 - 1;
+		turnInput = (float)neatOutputs [1] * 2 - 1;
+
+
+
+	}
+
+
+	public override void Stop ()
+	{
+		this._isRunning = false;
+	}
+
+	public override void Activate (IBlackBox box)
+	{
+		this._box = box;
+		this._isRunning = true;
+	}
+
+	public override float GetFitness ()
+	{
+		return _currSpeed;
+	}
+}
